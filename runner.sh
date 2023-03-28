6f435987021dbd12de8624212aa2a3d1c47433f0
Andrey <andrey.s.novosib@gmail.com>
2023-03-28 19:55:23 +0600

    add syslog for ui

--- runner.sh
+++ runner.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-while true; do python3 /home/anslyshik/tmp/py_proj/hard_monitor/ui.py -f /tmp/hard_monitor_ui_autorun; sleep 1; done
+while true; do
+  python3 /home/anslyshik/tmp/py_proj/hard_monitor/ui.py -f /tmp/hard_monitor_ui_autorun --logfile syslog
+  sleep 1
+done
